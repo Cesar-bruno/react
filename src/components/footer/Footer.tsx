9f0432c2dd8ce24fed3f2be592bdfc8030d94f19
Bruno Cesar <cesarbruno53@gmail.com>
2024-01-30 16:03:49 -0300

    atualizado final

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-﻿import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+﻿﻿import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -13,17 +13,14 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://github.com/conteudoGeneration/" target="_blank">
+                        <a href="https://github.com/Cesar-bruno" target="_blank">
                             <GithubLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                        <a href="https://www.linkedin.com/in/bcssilva/" target="_blank">
                             <LinkedinLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                        <a href="https://www.instagram.com/_brun.9/" target="_blank">
                             <InstagramLogo size={48} weight='bold' />
-                        </a>
-                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                            <FacebookLogo size={48} weight='bold' />
                         </a>
                     </div>
                 </div>
